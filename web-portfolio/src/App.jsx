e56f11b89fa27a603d7b2d924984b43674dd3391
NekoSakuraLucia <mokurolucia@gmail.com>
2025-03-11 10:11:17 +0700

    เปลี่ยน import path แต่ละคอมโพเนนต์ให้เป็น alias

--- web-portfolio/src/App.jsx
+++ web-portfolio/src/App.jsx
@@ -1,21 +1,19 @@
 import PropTypes from 'prop-types';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer/Footer';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer/Footer';
 
 function App({ children }) {
-  return (
-    <div className="min-h-screen bg-white dark:bg-neutral-950 transition-colors duration-300 flex flex-col">
-      <Navbar />
-      <main className="flex-grow">
-        {children}
-      </main>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="min-h-screen bg-white dark:bg-neutral-950 transition-colors duration-300 flex flex-col">
+            <Navbar />
+            <main className="flex-grow">{children}</main>
+            <Footer />
+        </div>
+    );
 }
 
 App.propTypes = {
-  children: PropTypes.node.isRequired,
+    children: PropTypes.node.isRequired,
 };
 
 export default App;
